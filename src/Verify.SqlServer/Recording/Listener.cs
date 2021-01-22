851f4b2ccf2364d2a1faa257670e0261a1716955
Simon Cropp <simon.cropp@gmail.com>
2021-01-22 19:52:34 +1100

    move logentry

--- src/Verify.SqlServer/Recording/Listener.cs
+++ src/Verify.SqlServer/Recording/Listener.cs
@@ -5,7 +5,7 @@ using System.Data.Common;
 using System.Diagnostics;
 using System.Threading;
 using Microsoft.Extensions.DiagnosticAdapter;
-using VerifyTests;
+using Verify.SqlServer;
 
 class Listener :
     IObserver<DiagnosticListener>,
--- src/Verify.SqlServer/Recording/LogEntry.cs
+++ src/Verify.SqlServer/Recording/LogEntry.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Common;
 
-namespace VerifyTests
+namespace Verify.SqlServer
 {
     public class LogEntry
     {
