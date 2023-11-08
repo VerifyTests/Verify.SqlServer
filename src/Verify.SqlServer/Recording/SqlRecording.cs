e31bbfa5c1a33425eeaaf70ae612f143d0fe018c
Simon Cropp <simon.cropp@gmail.com>
2023-11-08 23:05:23 +1100

    Recording

--- src/Verify.SqlServer/Recording/SqlRecording.cs
+++ src/Verify.SqlServer/Recording/SqlRecording.cs
@@ -1,14 +1,4 @@
 ﻿namespace VerifyTests;
 
 [Obsolete("Use VerifyTests.Recording")]
-public static class SqlRecording
-{
-    static Listener listener = new();
-
-    [ModuleInitializer]
-    public static void Initialize()
-    {
-        // ReSharper disable once UnusedVariable
-        var subscription = DiagnosticListener.AllListeners.Subscribe(listener);
-    }
-}
+public static class SqlRecording;
--- src/Verify.SqlServer/VerifySqlServer.cs
+++ src/Verify.SqlServer/VerifySqlServer.cs
@@ -5,6 +5,8 @@ namespace VerifyTests;
 
 public static class VerifySqlServer
 {
+    static Listener listener = new();
+
     public static bool Initialized { get; private set; }
 
     public static void Initialize()
@@ -30,6 +32,8 @@ public static class VerifySqlServer
 
         VerifierSettings.RegisterFileConverter<MsConnection>(ToSql);
         VerifierSettings.RegisterFileConverter<SysConnection>(ToSql);
+        // ReSharper disable once UnusedVariable
+        var subscription = DiagnosticListener.AllListeners.Subscribe(listener);
     }
 
     static ConversionResult ToSql(MsConnection connection, IReadOnlyDictionary<string, object> context)
